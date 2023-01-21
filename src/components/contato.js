56b9f746591c77985fe52256670b0710b30a8d2e
William Pscheidt <williampscheidt@hotmail.com>
2023-01-21 17:40:20 -0300

    Contact options

--- src/components/contato.js
+++ src/components/contato.js
@@ -1,11 +1,18 @@
+import emailIcon from './svg/email-icon.svg'
+
 const contato = () => {
     return (
         <section class="contato" id="contato">
             <h3 class="title">Contato</h3>
             <div class="social-media">
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin-icon" />
+                <img class="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin-icon" />
                 <span id="separator">·</span>
-                <a href="https://www.linkedin.com/in/william-pscheidt-9193341a4/">Acessar</a>
+                <a href="https://www.linkedin.com/in/william-pscheidt-9193341a4/">Acesse o meu perfil</a>
+            </div>
+            <div class="social-media">
+                <img class="icon" src={emailIcon} alt="linkedin-icon" />
+                <span id="separator">·</span>
+                <span>contato@williampscheidt.com.br</span>
             </div>
         </section>
     )
