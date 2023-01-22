a1c2f0565187caf07960b7a51c2a07eeb1413f6a
William Pscheidt <williampscheidt@hotmail.com>
2023-01-21 21:40:18 -0300

    Header changed

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,9 @@ const header = () => {
         <nav className="header" id="header">
             <a href="#header" className="active">/William</a>
             <span id="span-header">·</span>
-            <a href="#skills">/Skills</a>
-            <span id="span-header">·</span>
             <a href="#projetos">/Projetos</a>
             <span id="span-header">·</span>
-            <a href="#contato">/Contato</a>
+            <a href="https://github.com/WilliamPscheidt">/Github</a>
         </nav>
         <h1 className="my-name">William Pscheidt Polaski</h1>
         <h1 className="my-title">Back-end Developer</h1>
